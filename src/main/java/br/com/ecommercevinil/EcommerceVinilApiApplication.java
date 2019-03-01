2d4e9897f72fa8c52f103cb3b2cb41296fbdcda6
Luiz Rogerio Marcandalli <luiz.marcandalli@zarpsystem.com.br>
2019-02-28 21:35:41 -0300

    criando o projeto spring-boot

---
+++ src/main/java/br/com/ecommercevinil/EcommerceVinilApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.ecommercevinil;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommerceVinilApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcommerceVinilApiApplication.class, args);
+	}
+
+}
