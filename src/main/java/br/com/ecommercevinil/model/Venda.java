4104c793621a39416b67c43e7b86281535b13932
Luiz Rogerio Marcandalli <luiz.marcandalli@zarpsystem.com.br>
2019-02-28 23:32:07 -0300

    adicionando os models de Venda e VendaProduto

--- src/main/java/br/com/ecommercevinil/model/Venda.java
+++ src/main/java/br/com/ecommercevinil/model/Venda.java
@@ -1,4 +1,26 @@
 package br.com.ecommercevinil.model;
 
-public class Venda {
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class Venda implements Serializable {
+
+    private static final long serialVersionUID = 5832549185558688739L;
+
+    private Integer id;
+    private LocalDate data;
+    private BigDecimal valorTotal;
+    private BigDecimal valorTotalComCashBack;
+    private List<VendaProduto> vendaProduoList;
 }
--- src/main/java/br/com/ecommercevinil/model/VendaProduto.java
+++ src/main/java/br/com/ecommercevinil/model/VendaProduto.java
@@ -1,4 +1,25 @@
 package br.com.ecommercevinil.model;
 
-public class VendaProduto {
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class VendaProduto implements Serializable {
+
+    private static final long serialVersionUID = 4087975436377845929L;
+
+    private Integer id;
+    private Disco disco;
+    private Integer qtde;
+    private BigDecimal valor;
+    private BigDecimal valorComCashBack;
+
 }
