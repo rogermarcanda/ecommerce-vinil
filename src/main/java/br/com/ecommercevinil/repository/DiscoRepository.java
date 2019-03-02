d20c8086a8f29eb4e87c8f72b4add91581cf9574
Luiz Rogerio Marcandalli <luiz.marcandalli@zarpsystem.com.br>
2019-03-02 13:16:04 -0300

    adicionando anotações de query

--- src/main/java/br/com/ecommercevinil/repository/DiscoRepository.java
+++ src/main/java/br/com/ecommercevinil/repository/DiscoRepository.java
@@ -3,11 +3,15 @@ package br.com.ecommercevinil.repository;
 import br.com.ecommercevinil.model.Disco;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
 public interface DiscoRepository extends JpaRepository<Disco, Integer> {
 
-    //@Query("SELECT disco FROM Disco disco WHERE disco.genero.id =:id")
-    List<Disco> findByGenero(Integer idGenero, Pageable pageable);
+    @Query("SELECT e FROM #{#entityName} e WHERE e.genero.id = :idGenero")
+    List<Disco> findByGenero(
+            @Param("idGenero") Integer idGenero,
+            Pageable pageable);
 }
--- src/main/java/br/com/ecommercevinil/repository/VendaRepository.java
+++ src/main/java/br/com/ecommercevinil/repository/VendaRepository.java
@@ -3,11 +3,17 @@ package br.com.ecommercevinil.repository;
 import br.com.ecommercevinil.model.Venda;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.time.LocalDate;
 import java.util.List;
 
 public interface VendaRepository extends JpaRepository<Venda, Integer> {
 
-    List<Venda> findByBetweenData(LocalDate dataInicial, LocalDate dataFinal, Pageable pageable);
+    @Query("SELECT e FROM #{#entityName} e WhERE e.data BETWEEN :dataInicial AND :dataFinal")
+    List<Venda> findByBetweenData(
+            @Param("dataInicial") LocalDate dataInicial,
+            @Param("dataFinal") LocalDate dataFinal,
+            Pageable pageable);
 }
